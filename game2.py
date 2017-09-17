993ee914ac090a5a21cba1256d1a55890743228f
downs19 <downs.jamie@gmail.com>
2017-09-17 13:52:57 -0400

    shooter game

--- game2.py
+++ game2.py
@@ -1,5 +1,9 @@
 import pygame
+from pygame.sprite import Group, groupcollide
 from Player import Player
+from Bad_guy import Bad_guy
+from bullet import bullet
+
 
 pygame.init()
 
@@ -10,6 +14,11 @@ screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("EPIC SHOOTER")
 
 the_player = Player('joker.png',100,100, screen)
+bad_guy = Bad_guy(screen)
+bad_guys = Group()
+bad_guys.add(bad_guy)
+bullets = Group()
+
 
 # the_player_image = pygame.image.load('batman.png')
 # player = {
@@ -23,10 +32,11 @@ while game_on:
 
 
 	for event in pygame.event.get():
-		print event
+		# print event
 		if event.type == pygame.QUIT:
 			game_on = False
 		elif event.type == pygame.KEYDOWN:
+			print event.key
 			if event.key == 273:
 				# the_player.y -= the_player.speed
 				the_player.should_move('up',True)
@@ -39,6 +49,12 @@ while game_on:
 			elif event.key == 276:
 				# the_player.x -= the_player.speed
 				the_player.should_move('left',True)
+			elif event.key == 32:
+				
+				new_bullet = bullet(screen, the_player, 1)
+				bullets.add(new_bullet)
+				new_bullet = bullet(screen, the_player, 3)
+				bullets.add(new_bullet)
 
 		elif event.type == pygame.KEYUP:
 			if event.key == 273:
@@ -53,10 +69,20 @@ while game_on:
 			elif event.key == 276:
 				# the_player.x -= the_player.speed
 				the_player.should_move('left',False)	
-
+	#print bullets			
 	screen.fill(background_color)
-
+	for bad_guy in bad_guys:
+		bad_guy.update_me(the_player)
+	# Bad_guy.update_me(the_player)
+		bad_guy.draw_me()
 	the_player.draw_me()
+	
+	for bullet in bullets:
+		bullet.update()
+		bullet.draw_bullet()
+
+	bullet_hit = groupcollide(bullets,bad_guys,True,True)	
+
 	# screen.blit(the_player.image, [the_player.x, the_player.y])
 	pygame.display.flip()
 
